e2970f95a2466617fe7ab96e9038c26186e9796c
Hagar <hagarpaytan96@gmail.com>
2024-02-28 13:13:49 +0200

    added datasets

--- src/api_data_retrieve.py
+++ src/api_data_retrieve.py
@@ -1,19 +1,30 @@
-import requests
+# import requests
 
-url = "https://moviesdatabase.p.rapidapi.com/actors"
+# url = "https://moviesdatabase.p.rapidapi.com/actors"
 
-headers = {
-	"X-RapidAPI-Key": ,
-	"X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com"
-}
+# headers = {
+# 	"X-RapidAPI-Key": ,
+# 	"X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com"
+# }
 
-response = requests.get(url, headers=headers, params={})
+# response = requests.get(url, headers=headers, params={})
 
 
-if response.status_code == 200:
-    data = response.json()
-    # Process the data returned from the API
-else:
-    print("Error connecting to the API:", response.status_code)
+# if response.status_code == 200:
+#     data = response.json()
+#     # Process the data returned from the API
+# else:
+#     print("Error connecting to the API:", response.status_code)
     
-print(data)
+# print(data)
+
+import csv
+
+movies_csv = "imdb_top_1000.csv"
+
+with open(movies_csv, "r", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    
+
+
+
