a8f3ce54a51e56847ecb4e6f22f2018c2b21b571
Sophiep1103 <92567819+Sophiep1103@users.noreply.github.com>
2024-03-04 21:44:46 +0200

    Update create_db_script.py

--- src/create_db_script.py
+++ src/create_db_script.py
@@ -129,3 +129,103 @@ except mysql.connector.Error as err:
 finally:
     mycursor.close()
     mydb.close()
+
+
+
+
+######### more elegant way -Sophei 4.3 ############
+
+import mysql.connector
+from mysql.connector import errorcode
+
+try:
+    # Establishing connection to the MySQL server with out details
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="hagarleap",
+        password="hagar30040",
+        database="hagarleap",
+        port=3305
+    )
+    mycursor = mydb.cursor(buffered=True)
+
+   # Creating tables, if they don't exist
+    table_queries = [
+        """CREATE TABLE IF NOT EXISTS Movies (
+            id INT PRIMARY KEY, 
+            overview TEXT DEFAULT NULL, 
+            release_date DATE DEFAULT NULL, 
+            tagline TEXT, 
+            title VARCHAR(255) DEFAULT NULL, 
+            vote_avg FLOAT, 
+            vote_count INT, 
+            FULLTEXT INDEX idx_overview (overview),
+            FULLTEXT INDEX idx_tagline (tagline)
+        )""",
+        """CREATE TABLE IF NOT EXISTS Keywords (
+            id INT PRIMARY KEY, 
+            name VARCHAR(50) NOT NULL,
+            FULLTEXT INDEX idx_name (name)
+        )""",
+        """CREATE TABLE IF NOT EXISTS Movie_keywords (
+            movie_id INT, 
+            keyword_id INT, 
+            PRIMARY KEY (movie_id, keyword_id), 
+            FOREIGN KEY (movie_id) REFERENCES Movies(id), 
+            FOREIGN KEY (keyword_id) REFERENCES Keywords(id)
+        )""",
+        """CREATE TABLE IF NOT EXISTS Genres (
+            id INT PRIMARY KEY, 
+            name VARCHAR(255) NOT NULL
+        )""",
+        """CREATE TABLE IF NOT EXISTS Genres_movies (
+            movie_id INT, 
+            genre_id INT, 
+            PRIMARY KEY (movie_id, genre_id), 
+            FOREIGN KEY (movie_id) REFERENCES Movies(id), 
+            FOREIGN KEY (genre_id) REFERENCES Genres(id)
+        )""",
+        """CREATE TABLE IF NOT EXISTS Person (
+            id INT PRIMARY KEY, 
+            name VARCHAR(255), 
+            gender INT
+        )""",
+        """CREATE TABLE IF NOT EXISTS Cast (
+            cast_id INT PRIMARY KEY, 
+            FOREIGN KEY (cast_id) REFERENCES Person(id)
+        )""",
+        """CREATE TABLE IF NOT EXISTS Crew (
+            crew_id INT PRIMARY KEY, 
+            FOREIGN KEY (crew_id) REFERENCES Person(id)
+        )""",
+        """CREATE TABLE IF NOT EXISTS Actor_movies (
+            actor_id INT, 
+            movie_id INT, 
+            PRIMARY KEY (actor_id, movie_id), 
+            FOREIGN KEY (actor_id) REFERENCES Person(id), 
+            FOREIGN KEY (movie_id) REFERENCES Movies(id)
+        )""",
+        """CREATE TABLE IF NOT EXISTS MovieCrew (
+            movie_id INT, 
+            crew_id INT, 
+            job VARCHAR(100), 
+            PRIMARY KEY (movie_id, crew_id), 
+            FOREIGN KEY (movie_id) REFERENCES Movies(id), 
+            FOREIGN KEY (crew_id) REFERENCES Crew(crew_id)
+        )"""
+    ]
+
+    for query in table_queries:
+        table_name = query.split()[5] 
+        mycursor.execute(query)
+        print(f"---Table {table_name} has been created---")
+
+except mysql.connector.Error as err:
+    print("Failed creating database: {}".format(err))
+    exit(1)
+
+finally:
+    # Closing cursor and database connection
+    mycursor.close()
+    mydb.close()
+
